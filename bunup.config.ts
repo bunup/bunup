5721a3686a59594a6ebd53b1ac9ef808af4a3918
arshad <arshadpyaseen@gmail.com>
2025-08-29 23:22:30 +0530

    chore: update

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,28 +1,24 @@
-import { defineWorkspace } from './packages/bunup/src'
 import { exports, unused } from './packages/bunup/src/plugins'
 
-export default defineWorkspace(
-	[
-		{
-			name: 'bunup',
-			root: 'packages/bunup',
-			config: {
-				target: 'bun',
-				entry: ['src/index.ts', 'src/plugins.ts', 'src/cli/index.ts'],
-			},
-		},
-		{
-			name: '@bunup/dts',
-			root: 'packages/dts',
-			config: {
-				entry: ['src/index.ts'],
-			},
-		},
-	],
+export default [
 	{
+		name: 'bunup',
+		root: 'packages/bunup',
+		config: {
+			target: 'bun',
+			entry: ['src/index.ts', 'src/plugins.ts', 'src/cli/index.ts'],
+		},
 		plugins: [exports(), unused()],
 		dts: {
 			splitting: true,
 		},
 	},
-)
+	{
+		name: '@bunup/dts',
+		root: 'packages/dts',
+		plugins: [exports(), unused()],
+		config: {
+			entry: ['src/index.ts'],
+		},
+	},
+]

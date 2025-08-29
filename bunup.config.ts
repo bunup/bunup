64fff6de71bbf29c2ecf3e23c62b78f71c9f5176
arshad <arshadpyaseen@gmail.com>
2025-08-29 11:12:05 +0530

    fix: root build, move to bunup workspaces

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,10 +1,21 @@
+import { defineWorkspace } from './packages/bunup/src'
 import { exports, unused } from './packages/bunup/src/plugins'
 
-export default {
-	target: 'bun',
-	entry: ['src/index.ts', 'src/plugins.ts', 'src/cli/index.ts'],
-	dts: {
-		splitting: true,
+export default defineWorkspace(
+	[
+		{
+			name: 'bunup',
+			root: 'packages/bunup',
+			config: {
+				target: 'bun',
+				entry: ['src/index.ts', 'src/plugins.ts', 'src/cli/index.ts'],
+			},
+		},
+	],
+	{
+		plugins: [exports(), unused()],
+		dts: {
+			splitting: true,
+		},
 	},
-	plugins: [exports(), unused()],
-}
+)

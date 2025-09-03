71a4615c84329c5623ad0832e5266023ee3f5ef5
arshad <arshadpyaseen@gmail.com>
2025-09-03 10:08:35 +0530

    chore: fresh install packages

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,30 +1,31 @@
-import { defineWorkspace } from './packages/bunup/src'
 import { exports, unused } from './packages/bunup/src/plugins'
 
-export default defineWorkspace(
-	[
-		{
-			name: 'bunup',
-			root: 'packages/bunup',
-			config: {
-				target: 'bun',
-				entry: ['src/index.ts', 'src/plugins.ts', 'src/cli/index.ts'],
+export default [
+	{
+		name: 'bunup',
+		root: 'packages/bunup',
+		config: {
+			target: 'bun',
+			entry: ['src/index.ts', 'src/plugins.ts', 'src/cli/index.ts'],
+			dts: {
+				splitting: true,
 			},
 		},
-		{
-			name: '@bunup/dts',
-			root: 'packages/dts',
-		},
-		{
-			name: '@bunup/plugin-tailwind',
-			root: 'packages/plugin-tailwind',
-		},
-	],
+	},
 	{
-		entry: ['src/index.ts'],
-		plugins: [exports(), unused()],
-		dts: {
-			splitting: true,
+		name: '@bunup/dts',
+		root: 'packages/dts',
+		config: {
+			entry: ['src/index.ts'],
+			plugins: [exports(), unused()],
 		},
 	},
-)
+	{
+		name: '@bunup/plugin-tailwind',
+		root: 'packages/plugin-tailwind',
+		config: {
+			entry: ['src/index.ts'],
+			plugins: [exports(), unused()],
+		},
+	},
+]
